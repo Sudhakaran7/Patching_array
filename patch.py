e67c40a049c2abdd13235df33ab8a230da16e41d
Sudhakaran_roni <32321500+Sudhakaran7@users.noreply.github.com>
2020-09-13 07:34:16 +0530

    Create patch.py

---
+++ patch.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
+        patch, miss, i = 0, 1, 0
+        while miss <= n:
+            if i < len(nums) and nums[i] <= miss:
+                miss += nums[i]
+                i += 1
+            else:
+                miss += miss
+                patch += 1
+
+        return patch
+val=Solution()
+m,n=map(int,input().split())
+arr=list(map(int,input().split()))
+print(val.minPatches(arr,n))
